132eae92435eba0421885a7b22641ae8aabd3d41
Sariel RAQUIN <sarielr@hotmail.fr>
2023-01-26 15:02:31 +0100

    adding eleventy

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyconfig){
+
+    eleventyconfig.addPassthroughCopy('./src/style.css');
+    eleventyconfig.addPassthroughCopy('./src/images');
+    eleventyconfig.addPassthroughCopy('./src/script.js');
+    eleventyconfig.addPassthroughCopy('./src/uploads');
+        return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    };
+}

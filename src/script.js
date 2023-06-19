2b9440d7c11a9cd3e2baf477674f7eda449ddacd
Sariel RAQUIN <sarielr@hotmail.fr>
2023-06-19 23:51:32 +0200

    test

--- src/script.js
+++ src/script.js
@@ -43,9 +43,11 @@ onload = () => {
 
 
 
+const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
+
 const client = contentful.createClient({
   space: 'dj69i7y41zl2',
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+  accessToken: accessToken
 });
 
 // Récupérer les données des images depuis Contentful
